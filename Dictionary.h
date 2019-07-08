446867fa838bd0e280c9abc8d49bc1dc8e71e83d
SialSurion <noneisback@gmail.com>
2019-07-08 23:44:43 +0200

    Initial development of .txt file lines reading

--- Dictionary.h
+++ Dictionary.h
@@ -1,4 +1,18 @@
 #pragma once
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+struct asciiArtLetter
+{
+	vector <string> lines;
+	size_t letterWidth;
+};
+
 class Dictionary
 {
 	/*TODO:
@@ -10,8 +24,46 @@ class Dictionary
 	/*
 		std:string regex = "(?<character>[a-zA-Z0-9])\n(?<asciiArtLine1>.*)\n(?<asciiArtLine2>.*)\n(?<asciiArtLine3>.*)\n(?<asciiArtLine4>.*)\n(?<asciiArtLine5>.*)\n"
 	*/
-private:
-
 public:
+	map<char, asciiArtLetter> readCharToAsciiArtDictionary()
+	{
+		ifstream infile(".\alphabet.txt");
+
+		map<char, asciiArtLetter> dictionary;
+
+		const char *letter;
+		asciiArtLetter asciiLetter;
+
+		unsigned int linesRead = 0;
+		string line;
+		while (getline(infile, line) && linesRead < 6)
+		{
+			++linesRead;
+
+			if (linesRead == 1)
+			{
+				letter = line.c_str();
+			}
+			else
+			{
+				asciiLetter.lines.push_back(line);
+			}
+		}
+
+		//find longest
+		size_t width = 0;
+		for (auto& line : asciiLetter.lines)
+		{
+			if (width < line.size())
+			{
+				width = line.size();
+			}
+		}
+
+		asciiLetter.letterWidth = width;
+
+		return dictionary;
+	}
+
 
 };
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include "Dictionary.h"
 
+using namespace std;
+
 int main()
 {
+	Dictionary dict;
+	auto lettersDict = dict.readCharToAsciiArtDictionary();
 	return 0;
 }
